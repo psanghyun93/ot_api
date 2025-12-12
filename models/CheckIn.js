1817a1e4c6adc69822dce3eb3f1154062cbf98c3
sanghyun Park <p.sanghyun.93@gmail.com>
2025-12-12 15:56:52 +0900

    쿼리오류 컬럼명오타

--- models/CheckIn.js
+++ models/CheckIn.js
@@ -7,7 +7,7 @@ class CheckIn {
    * 체크인 기록 조회
    */
   static async findByUserId(userId) {
-    const result = db.query('SELECT * FROM users WHERE user_id = $1', [userId]);
+    const result = db.query('SELECT * FROM users WHERE id = $1', [userId]);
     if(!result.rows.length) {
       throw new Error(`User with id ${userId} not found`);
     }
